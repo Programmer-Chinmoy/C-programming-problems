c0349f3984315480cbeb1e6e2681933ffff940f7
Programmer-Chinmoy <84435019+Programmer-Chinmoy@users.noreply.github.com>
2022-02-06 01:26:15 +0600

    Add files via upload

---
+++ All Problems & solves/Program 2/program-2.c
@@ -0,0 +1,16 @@
+//program 2
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num > 50){
+        printf("The number is grater than 50.\n");
+    }
+
+    return 0;
+}
