906101efae4d288ff3ae4db14e79b71f82ce45a4
Programmer-Chinmoy <84435019+Programmer-Chinmoy@users.noreply.github.com>
2022-02-12 17:51:34 +0600

    Add files via upload

---
+++ All Problems & solves/Program 28/program28.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    FILE *input_file;
+
+    input_file = fopen("text.txt", "r");
+
+    char input[80];
+
+    fgets(input, 80, input_file);
+
+    puts(input);
+    
+    return 0;
+}
